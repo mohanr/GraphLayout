d65a2ae5e657b57ca530822222312a2fdaec56f6
Mohan Radhakrishnan <radhakrishnan.mohan@gmail.com>
2019-12-03 14:48:36 +0530

    Create GraphLayoutApplication.java

---
+++ com.graph.layout/src/main/java/com/graph/layout/GraphLayoutApplication.java
@@ -0,0 +1,27 @@
+package com.graph.layout;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class GraphLayoutApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/scene.fxml"));
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("/styles.css").toExternalForm());
+
+        stage.setTitle("Graph");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
