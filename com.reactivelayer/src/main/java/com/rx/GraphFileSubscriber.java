209c77031b7eb018ace82aa482e2ccaceaab70ae
Mohan Radhakrishnan <radhakrishnan.mohan@gmail.com>
2019-12-04 18:18:14 +0530

    Create GraphFileSubscriber.java

---
+++ com.reactivelayer/src/main/java/com/rx/GraphFileSubscriber.java
@@ -0,0 +1,22 @@
+package com.rx;
+
+import io.reactivex.observers.DisposableObserver;
+
+public class GraphFileSubscriber extends DisposableObserver {
+
+    @Override
+    public void onNext(Object o) {
+
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+
+    }
+
+    @Override
+    public void onComplete() {
+
+    }
+}
+
