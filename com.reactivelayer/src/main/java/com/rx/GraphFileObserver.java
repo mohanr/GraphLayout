8d3b12aafbded8f2d68b95b27d21dd10ac7518c7
Mohan Radhakrishnan <radhakrishnan.mohan@gmail.com>
2019-12-04 18:17:47 +0530

    Update GraphFileObserver.java

--- com.reactivelayer/src/main/java/com/rx/GraphFileObserver.java
+++ com.reactivelayer/src/main/java/com/rx/GraphFileObserver.java
@@ -1,14 +1,13 @@
 package com.rx;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-
-import io.reactivex.Observable;
-import org.reactivestreams.Subscriber;
+import io.reactivex.Flowable;
 
 
 public class GraphFileObserver {
+
+
+    public void fileOperator(){
+        Flowable<GraphFileObserver> f;
+    }
 
 }
